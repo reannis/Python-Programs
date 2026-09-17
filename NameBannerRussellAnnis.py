b228432f90857b78dd91481f7fed4f615fd424cc
reannis <pqbwsq9sms@privaterelay.appleid.com>
2026-09-17 16:22:15 -0400

    Update NameBannerRussellAnnis.py

--- NameBannerRussellAnnis.py
+++ NameBannerRussellAnnis.py
@@ -1,8 +1,22 @@
-print("R R R R R                            U                        U                   S S S S S S            S S S S S S                              A            ")
-print("R               R                         U                        U                   S                            S                                             AA          ")
-print("R                 R                       U                        U                   S                            S                                           A    A        ")
-print("R                R                        U                        U                   S S S S S               S SS S S                              A        A      ")
-print("R R R R R                            U                        U                                S                           S                            A A A A A    ")
-print("R             R                            U                     U                                  S                           S                           A                A  ")
-print("R                R                           U                 U                                    S                           S                          A                   A ")
-print("R                  R                           U U U U U                          S S S S S               S S S S S                        A                       A")
+'''
+
+ Name:Russell Annis
+ Date:9/17/2026
+ Course Number:CSC 113 D03
+ Course Name: intro to python programing
+ Problem Number: 1
+ Email: reannis2501@student.stcc.edu
+ Problem Description: create a program that prints my name as a banner
+
+I used python IDLE to program this, it looked weird when I uploaded it so in the editor I made it look like i did in the IDLE, 
+sorry if this makes the output weird
+
+''' 
+print("R R R R R               U                   U                   S S S S S S               S S S S S S                              A        ")
+print("R        R              U                   U                   S                         S                                        AA       ")
+print("R         R             U                   U                   S                         S                                      A    A     ")
+print("R        R              U                   U                   S S S S S                 S SS S S                              A      A    ")
+print("R R R R R               U                   U                             S                       S                            A A A A A    ")
+print("R         R              U                 U                              S                       S                           A         A   ")
+print("R           R              U             U                                S                       S                          A           A  ")
+print("R             R               U U U U U                          S S S S S               S S S S S                          A             A ")
